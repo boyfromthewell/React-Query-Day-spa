1eac5a91edaa905cc0c15a3e5a339b7200131808
boyfromthewell <tnsdyd10@naver.com>
2023-07-25 16:48:39 +0900

    feat: project init

---
+++ client/src/react-query/queryClient.ts
@@ -0,0 +1,21 @@
+import { createStandaloneToast } from '@chakra-ui/react';
+import { QueryCache, QueryClient } from '@tanstack/react-query';
+
+import { theme } from '../theme';
+
+const toast = createStandaloneToast({ theme });
+
+function queryErrorHandler(error: unknown): void {
+  // error is type unknown because in js, anything can be an error (e.g. throw(5))
+  const title =
+    error instanceof Error ? error.message : 'error connecting to server';
+
+  toast({ title, status: 'error', variant: 'subtle', isClosable: true });
+}
+
+// to satisfy typescript until this file has uncommented contents
+export const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: queryErrorHandler,
+  }),
+});
