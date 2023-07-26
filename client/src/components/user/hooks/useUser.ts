e984d7b9f51853afd71094d93913bf62d2e3095d
boyfromthewell <tnsdyd10@naver.com>
2023-07-26 19:32:13 +0900

    feat: login & logout & get user appointments

--- client/src/components/user/hooks/useUser.ts
+++ client/src/components/user/hooks/useUser.ts
@@ -1,3 +1,4 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 
 import type { User } from '../../../../../shared/types';
@@ -9,17 +10,17 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
 
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 interface UseUser {
   user: User | null;
   updateUser: (user: User) => void;
@@ -27,17 +28,25 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
-  const user = null;
-
+  const queryClient = useQueryClient();
+  // 기존 user 값을 사용해 서버에서 user 데이터 가져오기
+  const { data: user } = useQuery([queryKeys.user], () => getUser(user), {
+    // 초기 데이터를 캐시에 추가
+    initialData: getStoredUser,
+  });
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
     // TODO: update the user in the query cache
+    queryClient.setQueryData([queryKeys.user], newUser);
+    setStoredUser(newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
     // TODO: reset user to null in query cache
+    queryClient.setQueryData([queryKeys.user], null);
+    queryClient.removeQueries(['user-appointments']);
+    clearStoredUser();
   }
 
   return { user, updateUser, clearUser };
